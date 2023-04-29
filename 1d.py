e2ebe9728820575e2a7761fcccb6a45e32f87c98
maneesha <97738136+Mani1881@users.noreply.github.com>
2023-04-29 21:06:17 +0530

    Add files via upload

---
+++ 1d.py
@@ -0,0 +1,11 @@
+import sys
+n=len(sys.argv)
+#print(n)
+print(sys.argv[0])
+for i in range(1,n):
+    print(sys.argv[i],end='\n')
+s=0
+for i in range(1,n):
+    s+=int(sys.argv[i])
+print('sum=',s)
+    
