9de4ab733ffaab4b0219bee66ae48d67e83c15c3
maneesha <97738136+Mani1881@users.noreply.github.com>
2023-04-29 19:29:01 +0530

    Update Area.java

--- Area.java
+++ Area.java
@@ -4,5 +4,15 @@ public static void main(String[] args) {
         double r=15.5;
         double area=PI*r*r;
         System.out.println("Area of circle="+area);
+    //create NumberFormat class object
+    //getNumberInstance() is a factory method
+   NumberFormat obj=NumberFormat.getNumberInstance();
+    //store format information into obj
+    obj.setMaximumFractiondigits(2);
+    obj.setMinimumIntegerDigits(7);
+    //apply format to area value
+    String s=obj.format(area);
+    System.out.println("Formatted area="+s);
+    
 }
 }
