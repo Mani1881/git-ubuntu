c09118c423c94cec0c9cf87d26283eca092f5e5b
git-ubuntu <manumumba1881@gmail.com>
2023-04-29 19:17:19 +0530

    comitted

---
+++ Area.java
@@ -0,0 +1,8 @@
+public class Area{
+public static void main(String[] args) {
+    final double PI=(double)22/7;//constant
+        double r=15.5;
+        double area=PI*r*r;
+        System.out.println("Area of circle="+area);
+}
+}
