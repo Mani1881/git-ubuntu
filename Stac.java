bc792f262e5e892d9c0aad428e72aa5977a4c02a
maneesha <97738136+Mani1881@users.noreply.github.com>
2023-04-30 14:51:58 +0530

    Update Stac.java

--- Stac.java
+++ Stac.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
-//import javax.lang.model.util.ElementScanner6;
 public class Stac{
     public static void main(String[] args)throws IOException {
 //create a empty stack to contain Integer objects 
