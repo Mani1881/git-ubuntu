44c38024398a408691899fdafd18a1e3543c021d
maneesha <97738136+Mani1881@users.noreply.github.com>
2023-05-04 20:35:13 +0530

    Add files via upload

---
+++ str.java
@@ -0,0 +1,14 @@
+public class str {
+    public static void main(String[] args)
+{
+    String i[]=new String[4];
+    i[0]="manu";
+    i[2]="iou";
+    System.out.println(i[0]+i[2]);
+    System.out.println("**************");
+String k[]={"tryeur","ryyr","tryey","ipouj"};
+for(String h:k){
+    System.out.println(h);
+}
+}
+}
